f256dc4473044b5b2a0464887dd7c83db9f0bb08
PAenugula <paenugul@calpoly.edu>
2020-02-09 03:37:46 -0800

    added recipe activities

---
+++ app/src/main/java/com/cph2020/wirelesswater/InflateRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.cph2020.wirelesswater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InflateRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inflate_recipe);
+    }
+}
